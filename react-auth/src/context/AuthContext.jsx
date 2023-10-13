90d3c751c9208bf62800f6e2f50e971afa386ade
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-10-14 00:33:36 +0500

    ok

--- react-auth/src/context/AuthContext.jsx
+++ react-auth/src/context/AuthContext.jsx
@@ -1,59 +1,83 @@
-import { createContext,useContext,useState } from "react";
+import { createContext, useContext, useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext({});
+// const LOGIN_STORAGE_KEY = "login";
 
 export const AuthProvider = ({ children }) => {
-  const [user,setUser] = useState(null)
-  const [errors,setErrors] = useState([])
+  const [user, setUser] = useState();
+  const [errors, setErrors] = useState([]);
+  // const [isLogin, setIsLogin] = useState(false);
+
+  // const localLogin =
+  //   (typeof window !== "undefined" &&
+  //     JSON.parse(localStorage.getItem(LOGIN_STORAGE_KEY))) ||
+  //   [];
+  // const [islocalLogin, setIsLocalLogin] = useState(localLogin);
+
+  // useEffect(() => {
+  //   localStorage.setItem(LOGIN_STORAGE_KEY, JSON.stringify(islocalLogin));
+  // }, [islocalLogin]);
+  // useEffect(() => {
+  //   setIsLogin(islocalLogin);
+  // }, []);
 
   const navigate = useNavigate();
 
-  const csrf = () => axios.get('/sanctum/csrf-cookie');
-  
+  const csrf = () => axios.get("/sanctum/csrf-cookie");
+
   const getUser = async () => {
-    const {data} = await axios.get("/api/user");
-     setUser(data);
-  }
+    const { data } = await axios.get("/api/user");
+    console.log(data);
+    setUser(data);
+  };
 
-  const login = async ({ ...data }) =>{
+  const login = async ({ ...data }) => {
     await csrf();
-    try{
-      await axios.post('/login', data)
-      await getUser()
-      navigate("/")
-    }catch(e){
-      if(e.response.status === 422){
-        setErrors(e.response.data.errors);
-      }
-    }
-  }
-  const register = async ({ ...data }) =>{
-    await csrf();
-    try{
-      await axios.post('/register', data)
-      await getUser()
-      navigate("/")
-    }catch(e){
-      if(e.response.status === 422){
-        setErrors(e.response.data.errors);
-      }
-    }
-  }
-
-  const logout = ()=>{
-    axios.post("/logout").then(()=>{
-      setUser = (null);
+    try {
+      await axios.post("/login", data);
+      await getUser();
+      // setIsLocalLogin(true);
+      // setIsLogin(true);
       navigate("/");
-    })
-  }
+    } catch (e) {
+      if (e.response.status === 422) {
+        setErrors(e.response.data.errors);
+      }
+    }
+  };
+  const register = async ({ ...data }) => {
+    await csrf();
+    try {
+      await axios.post("/register", data);
+      await getUser();
+      // setIsLogin(true)
+      navigate("/");
+    } catch (e) {
+      if (e.response.status === 422) {
+        setErrors(e.response.data.errors);
+      }
+    }
+  };
 
-  return <AuthContext.Provider value={{ user, errors, getUser, login, register, logout}}>
-    {children}
-  </AuthContext.Provider>
-}
+  const logout = () => {
+    axios.post("/logout").then(() => {
+      setUser(null);
+      // setIsLocalLogin(false);
+      // setIsLogin(false);
+    });
+  };
 
-export default function useAuthContext(){
+  return (
+    <AuthContext.Provider
+      value={{ user, errors, getUser, login, register, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function useAuthContext() {
   return useContext(AuthContext);
 }
--- react-auth/src/pages/Home.jsx
+++ react-auth/src/pages/Home.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react"
-import useAuthContext from "../context/AuthContext"
+import { useEffect } from "react";
+import useAuthContext from "../context/AuthContext";
 
 function Home() {
   const { user, getUser } = useAuthContext();
 
+    useEffect(() => {
+      if (!user) {
+        getUser();
+      }
+    }, []);
 
   return (
-    <div className='max-w-6xl mx-auto'>
-      {user && <div>{user?.name}</div>}
-    </div>
-  )
+    <div className="max-w-6xl mx-auto">{user?.name}</div>
+  );
 }
 
-export default Home
+export default Home;
